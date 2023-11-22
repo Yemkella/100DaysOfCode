73c8453be208596a77375c3646a1bcc8a46e6510
Jose Rodriguez <jose_rodriguez0@me.com>
2023-11-22 12:00:45 -0600

    Added day 25 mail merge project and updated snake game

--- 020-021-Snake/scoreboard.py
+++ 020-021-Snake/scoreboard.py
@@ -1,28 +1,38 @@
 from turtle import Turtle
-FONT = ("Courier", 20, "normal")
-ALIGNMENT = "center"    
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
 class Scoreboard(Turtle):
-    
+
     def __init__(self):
         super().__init__()
-        self.penup()
+        with open("data.txt", mode='r') as file:
+            self.high_score = int(file.read())
         self.score = 0
-        self.hideturtle()
         self.color("white")
-        self.goto(0, 275)
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
-
-
-    def count(self):
-        self.clear()
         self.penup()
-        self.score += 1
+        self.goto(0, 270)
         self.hideturtle()
-        self.color("white")
-        self.goto(0, 275)
-        self.write(f"Score: {self.score}", False, align="center", font=("Arial", 20, "normal"))
+        self.update_scoreboard()
 
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"Game Over", False, align="center", font=("Arial", 20, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode='w') as file:
+                file.write(f"{self.score}")
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
