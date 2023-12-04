fd28ec6cecd30cbfee2a8cf0429b3f99b541736d
Jose Rodriguez <jose_rodriguez0@me.com>
2023-12-04 14:46:03 -0600

    Day 36 added

---
+++ 036-Stock/main.py
@@ -0,0 +1,35 @@
+import requests
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_KEY = "redacted"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_KEY = "redacted"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+diff_percent = (difference / yesterday_closing_price) * 100
+
+if diff_percent > 5:
+    news_params = {
+        "apiKey": NEWS_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+    print(three_articles)
+
